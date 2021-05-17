635cbe0b94e9f8a34cce0082fdb64dd202847add
Yourownway <lorris.dhedin@hotmail.fr>
2021-05-17 14:49:11 +0200

    feat(src/js/App.jsx): insert store

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state.app);
+
+  useEffect(() => {
+    dispatch({ type: "APP_INIT" });
+
+    setTimeout(() => {
+      dispatch({ type: "APP_READY" });
+    }, 2000);
+  }, []);
+
+  if (state.loading === true) return <div>Loading</div>;
   return <div>Hello World</div>;
 };
 
