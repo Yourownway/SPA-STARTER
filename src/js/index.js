4137fef51e37dd308416e57a199818158939f0ac
Yourownway <lorris.dhedin@hotmail.fr>
2021-05-17 14:47:46 +0200

    feat(src/js/index.js): init provider

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
-
+import configureStore from "./store/store";
 const INSERT_DIV = document.getElementById("app");
 
-ReactDOM.render(<App />, INSERT_DIV);
+const store = configureStore();
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  INSERT_DIV
+);
