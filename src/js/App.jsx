d643530c0cdef0fc75a45b31f43d2d7099433576
Yourownway <lorris.dhedin@hotmail.fr>
2021-05-18 12:42:46 +0200

    feat(global): add Router and layout

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styled, { css, keyframes } from "styled-components";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Books from "./components/pages/Books";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import AuthLayout from "./components/templates/AuthLayout";
+import MainLayout from "./components/templates/MainLayout";
+import Header from "./components/molecules/Header";
+import "./style/globalStyle.css";
 
 const rotate = keyframes`
 from{
@@ -13,7 +23,9 @@ to {
 
 const Title = styled.h1`
   font-size: 1.5em;
-  text-align: center;
+  margin: auto;
+  width: 30%;
+  text-align: right;
 
   margin-top: 100px;
   &::before {
@@ -48,8 +60,46 @@ const App = () => {
 
   return (
     <>
+      <Header />
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/books">Books</Link>
+            </li>
+            <li>
+              <Link to="/register"> Register</Link>
+            </li>
+            <li>
+              <Link to="/login"> Login </Link>
+            </li>
+          </ul>
+        </div>
+        <Switch>
+          <MainLayout>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/books">
+              <Books />
+            </Route>
+          </MainLayout>
+        </Switch>
+        <AuthLayout>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </AuthLayout>
+      </Router>
+      {/* <Nav></Nav>
       <h1> Welcome to React Redux </h1>
-      <button onClick={() => handleClick()}>Reload</button>
+      <button onClick={() => handleClick()}>Reload</button> */}
     </>
   );
 };
